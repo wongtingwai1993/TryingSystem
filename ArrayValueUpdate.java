630cffd5f18980c76935aab3e7b6ac6ffa80ce91
wongtingwai1993 <wongtingwai1993@gmail.com>
2019-12-02 11:22:46 +0800

    - GCD

---
+++ ArrayValueUpdate.java
@@ -0,0 +1,25 @@
+package com.sgx.regsub.common.validation;
+
+public class ArrayValueUpdate
+{
+    
+    public static int searchGreatValue(int totalNumber, int totalValue, int timesValue){
+        if(totalNumber *  timesValue > totalValue ){
+            return totalNumber;
+        }
+        return searchGreatValue(totalNumber +1, totalValue, timesValue);
+    }
+    
+    public static void main(String args[] ) throws Exception {
+        //BufferedReader
+        int totalNumber = 5;                // Reading input from STDIN
+        String[] splitValue = "1 2 3 4 5".split(" ");
+        int [] intValues = new int[splitValue.length];
+        int totalValue = 0;
+
+        for(int x =0; x < splitValue.length; x++){
+            totalValue += Integer.parseInt( splitValue[x] ) ;
+        }
+        System.out.println(searchGreatValue(totalValue/ totalNumber, totalValue,totalNumber));
+    }
+}
