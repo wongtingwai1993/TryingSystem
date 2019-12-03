08fb6d25ad62e48307a55e260f0d0178679ccf93
wongtingwai1993 <wongtingwai1993@gmail.com>
2019-12-04 00:55:28 +0800

    Return value based on character

---
+++ CharacterTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class CharacterTest {
+
+	
+	public static void main(String [] args) {
+		String s = "c";
+		char c = s.charAt(0);
+		int asciiValue = c;
+		
+		System.out.println(asciiValue);
+		
+	}
+}
